224e638ae987bd6ee82d9d33dfc87867a5a7c1d8
actuallyasmartname <77011692+actuallyasmartname@users.noreply.github.com>
2021-03-03 11:09:08 -0500

    Update x2Craft-FabricMain.java

--- src/main/java/net/actuallyasmartname/x2Craft-Fabric/init/x2Craft-FabricMain.java
+++ src/main/java/net/actuallyasmartname/x2Craft-Fabric/init/x2Craft-FabricMain.java
@@ -1,1 +1,10 @@
 package net.actuallyasmartname.x2Craft-Fabric.init;
+
+import net.fabricmc.api.ModInitializer;
+
+public class x2Craft_Fabric implements ModInitializer {
+	@Override
+	public void onInitialize() 
+    System.out.println("Systems Loading");
+	}
+}
