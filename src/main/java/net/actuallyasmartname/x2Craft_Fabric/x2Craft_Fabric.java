c22504a4d13789cabc7ff7822a16b9723338610c
actuallyasmartname <77011692+actuallyasmartname@users.noreply.github.com>
2021-03-04 18:16:53 -0500

    0.0.1a-rc1 - not for public use

--- src/main/java/net/actuallyasmartname/x2Craft_Fabric/x2Craft_Fabric.java
+++ src/main/java/net/actuallyasmartname/x2Craft_Fabric/x2Craft_Fabric.java
@@ -1,9 +1,15 @@
 package net.actuallyasmartname.x2Craft_Fabric;
+import net.minecraft.util.registry.Registry;
 import net.fabricmc.api.ModInitializer; 
-
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
 public class x2Craft_Fabric implements ModInitializer {
+    public static final Item FABRIC_ITEM = new Item(new Item.Settings().group(ItemGroup.MISC));
+
  @Override
  public void onInitialize() {
      System.out.println("Hello Fabric world!");
+     Registry.register(Registry.ITEM, new Identifier("x2craft", "fabric_item"), FABRIC_ITEM);
  }  
 }
