c0e7119726af1def3cde8048b578976ab965714c
actuallyasmartname <77011692+actuallyasmartname@users.noreply.github.com>
2021-03-04 20:29:55 -0500

    Unstable 0.0.1a-rc1-nightly

--- src/main/java/net/actuallyasmartname/x2Craft_Fabric/x2Craft_Fabric.java
+++ src/main/java/net/actuallyasmartname/x2Craft_Fabric/x2Craft_Fabric.java
@@ -1,15 +1,16 @@
 package net.actuallyasmartname.x2Craft_Fabric;
 import net.minecraft.util.registry.Registry;
+import net.actuallyasmartname.x2Craft_Fabric.item.PortableJukeboxPigstep;
 import net.fabricmc.api.ModInitializer; 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 public class x2Craft_Fabric implements ModInitializer {
-    public static final Item FABRIC_ITEM = new Item(new Item.Settings().group(ItemGroup.MISC));
+    public static final Item FABRIC_ITEM = new PortableJukeboxPigstep(new Item.Settings().group(ItemGroup.MISC));
 
  @Override
  public void onInitialize() {
-     System.out.println("Hello Fabric world!");
-     Registry.register(Registry.ITEM, new Identifier("x2craft", "fabric_item"), FABRIC_ITEM);
+     System.out.println("Systems Loading");
+     Registry.register(Registry.ITEM, new Identifier("x2craft", "portablejukeboxpigstep"), FABRIC_ITEM);
  }  
 }
